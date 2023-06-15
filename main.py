13f39303b2ae08bacf6c93720ca4b3e2205e18f1
yuhu-k <kyh08436.cs08@nctu.edu.tw>
2023-06-16 00:22:07 +0800

    fix some issue

--- main.py
+++ main.py
@@ -1,38 +1,96 @@
 import src.map_gen as mg
 import re
 
-def reverse(a,b):
-    d = (a ** 2 + b ** 2) ** (1/2)
-    e = ((500 - b) ** 2 + a ** 2 ) ** (1/2)
-    return (d,e)
+#def reverse(a,b):
+#    d = (a ** 2 + b ** 2) ** (1/2)
+#    e = ((500 - b) ** 2 + a ** 2 ) ** (1/2)
+#    return (d,e)
+#
+#
+#def main():
+#    map = mg.map_gen(500)
+#    coordinate = []
+#    f = open('test.txt','r')
+#    for data in f.readlines():
+#        if data.strip() == '':
+#            break
+#        else:
+#            data = data.strip()
+#            co = re.findall(r'[\d.]+',data)
+#            coordinate.append((float(co[0]) * 100,float(co[1]) * 100))
+#    f.close()
+#
+#    for coor in coordinate:
+#        dis = reverse(coor[0],coor[1])
+#        map.add_loca(dis[0],dis[1])
+#    dis = reverse(100,100)
+#    map.add_obs(dis[0],dis[1])
+#    dis = reverse(102,105)
+#    map.add_obs(dis[0],dis[1])
+#    
+#    map.draw_map()
 
+
+STEP_LENGTH = 0.15 # m
+
+def dfs(map:mg.map_gen):
+    rotate_angle = 0
+
+    now_location = getLocation()
+    map.add_loca(now_location)
+    map.update_car_coordination(now_location)
+    tmp_direction = ['front','left front','left','left back','back','right back','right','right front']
+
+    ind = 0
+    for direction in tmp_direction:
+        if map.is_not_get_place(now_location,direction=direction,length=STEP_LENGTH):
+            rotateCounterclockwise(ind*45 - rotate_angle)
+            map.update_car_direction(ind*45 - rotate_angle)
+            rotate_angle = ind*45
+            walk_length = 0
+            for i in range(STEP_LENGTH*100):
+                if not faceObstacle():
+                    moveForward(0.01)
+                    walk_length += 0.01
+                else:
+                    map.add_obs(getLocation(),'front',0.01)
+                    break
+            dfs(map)
+
+            for i in range(walk_length):
+                moveBackward(0.01)
+            map.update_car_coordination(getLocation())
+
+        ind += 1
+
+    if 360 - rotate_angle > rotate_angle:
+        rotateCounterclockwise(-rotate_angle)
+    else:
+        rotateCounterclockwise(360 - rotate_angle)
+        
+    
 
 def main():
-    map = mg.map_gen(500)
-    coordinate = []
-    f = open('test.txt','r')
-    for data in f.readlines():
-        if data.strip() == '':
-            break
-        else:
-            data = data.strip()
-            co = re.findall(r'[\d.]+',data)
-            coordinate.append((float(co[0]) * 100,float(co[1]) * 100))
-    f.close()
+    dis = getServersDistance()
 
-    last = [coordinate[0][0],coordinate[0][1]]
-    for coor in coordinate:
-        dis = reverse(coor[0],coor[1])
-        dis_last = reverse(last[0],last[1])
-        map.add_loca(dis[0],dis[1],dis_last[0],dis_last[1])
-        last = [coor[0],coor[1]]
-    dis = reverse(100,100)
-    map.add_obs(dis[0],dis[1])
-    dis = reverse(102,105)
-    map.add_obs(dis[0],dis[1])
     
+    if not faceObstacle():
+        back_location = getLocation()
+        moveForward(STEP_LENGTH)
+        front_location = getLocation()
+    else:
+        front_location = getLocation()
+        moveBackward(STEP_LENGTH)
+        back_location = getLocation()
+
+    
+    map = mg.map_gen(dis,back_location,front_location)
+
+    dfs(map)
+
     map.draw_map()
-        
+
+
 
 if __name__ == '__main__':
     main()
--- src/map_gen.py
+++ src/map_gen.py
@@ -1,5 +1,8 @@
 import numpy as np
 from PIL import Image
+import math.cos as cos
+import math.sin as sin
+import math.pi as pi
 
 
 MAP_NOT_GET_HERE = [192,192,192]
@@ -24,7 +27,7 @@ SPEED = 40
 
 class map_gen:
 
-    def __init__(self,dis:float) -> None:
+    def __init__(self,dis:float,back_dis_pair:list[float],front_dis_pair:list[float]) -> None:
         self.servers_distance = dis
         distance_coor = int(dis / BLOCK_SIZE)
         if distance_coor % 2 == 1:
@@ -45,43 +48,61 @@ class map_gen:
         self.map[self.server_a_coor[0]][self.server_a_coor[1]] = MAP_GATEWAY_DEVICE
         self.map[self.server_b_coor[0]][self.server_b_coor[1]] = MAP_GATEWAY_DEVICE
 
+        back_position_coordination = self.__cal_location(back_dis_pair[0],back_dis_pair[1])
+        front_position_coordination = self.__cal_location(front_dis_pair[0],front_dis_pair[1])
+
+        self.car_direction = np.array(front_position_coordination) - np.array(back_position_coordination)
+        self.car_direction = np.mat([self.car_direction[0]/((self.car_direction[0]**2 + self.car_direction[1]**2)**(1/2)),\
+                              self.car_direction[1]/((self.car_direction[0]**2 + self.car_direction[1]**2)**(1/2))])
+
+
         self.car_now_coor = None
 
-    def add_loca(self,dis_a:float,dis_b:float) -> None:
+    def update_car_coordination(self,coordination:np.mat):
+        self.car_now_coor = coordination
+
+    def update_car_direction(self,angle:int):
+        self.car_direction = np.matrix([[cos(angle),-sin(angle)],[sin(angle),cos(angle)]]) * self.car_direction
+
+
+    def add_loca(self,dis_pair:list[float]) -> None:
+        dis_a = dis_pair[0]
+        dis_b = dis_pair[1]
+
         coor = self.__get_coordination(dis_a,dis_b)
-        if coor[0] >= self.map_width:
-            tmp_np = np.zeros((2*(coor[0] - self.map_width + 1),self.map_length,3),dtype=np.uint8)
-            for i in range(2*(coor[0] - self.map_width + 1)):
+        if coor[0][0] >= self.map_width:
+            tmp_np = np.zeros((2*(coor[0][0] - self.map_width + 1),self.map_length,3),dtype=np.uint8)
+            for i in range(2*(coor[0][0] - self.map_width + 1)):
                 for j in range(self.map_length):
                     tmp_np[i][j] = MAP_NOT_GET_HERE
             self.map = np.row_stack((self.map,tmp_np))
-            self.map_width += 2 * (coor[0] - self.map_width + 1)
-        elif coor[1] >= self.map_length:
-            tmp_np = np.zeros((self.map_width,2 * (coor[1] - self.map_length + 1),3),dtype=np.uint8)
+            self.map_width += 2 * (coor[0][0] - self.map_width + 1)
+        elif coor[1][0] >= self.map_length:
+            tmp_np = np.zeros((self.map_width,2 * (coor[1][0] - self.map_length + 1),3),dtype=np.uint8)
             for i in range(self.map_width):
-                for j in range(2 * (coor[1] - self.map_length + 1)):
+                for j in range(2 * (coor[1][0] - self.map_length + 1)):
                     tmp_np[i][j] = MAP_NOT_GET_HERE
             self.map = np.column_stack((self.map,tmp_np))
-            self.map_length += 2 * (coor[1] - self.map_length + 1)
+            self.map_length += 2 * (coor[1][0] - self.map_length + 1)
 
-        if coor[0] < 0:
-            tmp_np = np.zeros((2*(0 - coor[0]),self.map_length,3),dtype=np.uint8)
-            for i in range(2*(0 - coor[0])):
+        if coor[0][0] < 0:
+            tmp_np = np.zeros((2*(0 - coor[0][0]),self.map_length,3),dtype=np.uint8)
+            for i in range(2*(0 - coor[0][0])):
                 for j in range(self.map_length):
                     tmp_np[i][j] = MAP_NOT_GET_HERE
             self.map = np.row_stack((tmp_np, self.map))
-            self.map_width += 2*(0 - coor[0])
-            self.server_a_coor[0] += 2*(0 - coor[0])
-            self.server_b_coor[0] += 2*(0 - coor[0])
-        elif coor[1] < 0:
-            tmp_np = np.zeros((self.map_width,2 * (0 - coor[1]),3),dtype=np.uint8)
+            self.map_width += 2*(0 - coor[0[0]])
+            self.server_a_coor[0][0] += 2*(0 - coor[0][0])
+            self.server_b_coor[0][0] += 2*(0 - coor[0][0])
+        elif coor[1][0] < 0:
+            tmp_np = np.zeros((self.map_width,2 * (0 - coor[1][0]),3),dtype=np.uint8)
             for i in range(self.map_width):
-                for j in range(2 * (0 - coor[1])):
+                for j in range(2 * (0 - coor[1][0])):
                     tmp_np[i][j] = MAP_NOT_GET_HERE
             self.map = np.column_stack((tmp_np,self.map))
-            self.map_length += 2 * (0 - coor[1])
-            self.server_a_coor[1] += 2 * (0 - coor[1])
-            self.server_b_coor[1] += 2 * (0 - coor[1])
+            self.map_length += 2 * (0 - coor[1][0])
+            self.server_a_coor[1][0] += 2 * (0 - coor[1][0])
+            self.server_b_coor[1][0] += 2 * (0 - coor[1][0])
 
 
         if self.car_now_coor:
@@ -89,50 +110,108 @@ class map_gen:
         else:
             coor_last = coor
         # get coordiate from distance
-        self.car_now_coor = coor
 
-        times = int(((coor[0] - coor_last[0]) ** 2 + (coor[1] - coor_last[1]) ** 2) ** (1/2) / (BLOCK_SIZE/100))
+        times = int(((coor[0][0] - coor_last[0][0]) ** 2 + (coor[1][0] - coor_last[1][0]) ** 2) ** (1/2) / (BLOCK_SIZE/100))
         if times == 0:
-            if list(self.map[coor[0]][coor[1]]) == MAP_NOT_GET_HERE:
-                self.map[coor[0]][coor[1]] = MAP_WALKABLE
+            if list(self.map[coor[0][0]][coor[1][0]]) == MAP_NOT_GET_HERE:
+                self.map[coor[0][0]][coor[1][0]] = MAP_WALKABLE
             return
-        x_len = (coor[0] - coor_last[0])/times
-        y_len = (coor[1] - coor_last[1])/times
+        x_len = (coor[0][0] - coor_last[0][0])/times
+        y_len = (coor[1][0] - coor_last[1][0])/times
         for i in range(times+1):
-            if list(self.map[int(coor_last[0] + x_len * i)][int(coor_last[1] + y_len * i)]) == MAP_NOT_GET_HERE:
-                self.map[int(coor_last[0] + x_len * i)][int(coor_last[1] + y_len*i)] = MAP_WALKABLE
+            if list(self.map[int(coor_last[0][0] + x_len * i)][int(coor_last[1][0] + y_len * i)]) == MAP_NOT_GET_HERE:
+                self.map[int(coor_last[0][0] + x_len * i)][int(coor_last[1][0] + y_len*i)] = MAP_WALKABLE
 
 
-    def add_obs(self,dis_a:float,dis_b:float):
-        coor = self.__get_coordination(dis_a,dis_b)
+    def add_obs(self,dis_pair:list[float]=None,direction='front',length=0):
+        if dis_pair:
+            coor = self.__get_coordination(dis_pair[0],dis_pair[1])
+        elif self.car_now_coor:
+            coor = self.car_now_coor
+        else:
+            print('error, need start location or input a pair of distance')
+            return
+
+        if length != 0:
+            if direction == 'front':
+                angle = 0
+            elif direction == 'left front':
+                angle = pi/4
+            elif direction == 'left':
+                angle = pi/2
+            elif direction == 'left back':
+                angle = pi*3/4
+            elif direction == 'back':
+                angle = pi
+            elif direction == 'right back':
+                angle = pi*5/4
+            elif direction == 'right':
+                angle = pi*3/2
+            elif direction == 'right front':
+                angle = pi*7/4
+
+            rotate_matrix = np.matrix([[cos(angle),-sin(angle)],[sin(angle),cos(angle)]])
+
+            tmp_direction = rotate_matrix * self.car_direction
+
+            coor += length * tmp_direction
+
         # get coordiate from distance
-        self.map[coor[0]][coor[1]] = MAP_NONWALKABLE
+        self.map[coor[0][0]][coor[1][0]] = MAP_NONWALKABLE
 
     def draw_map(self):
         im = Image.fromarray(np.flip(self.map.transpose((1,0,2)),(0)))
         im.save("test.png")
 
+    def is_not_get_place(self,dis_pair:list[float]=None,direction='front',length=0) -> bool:
+        if dis_pair:
+            coor = self.__get_coordination(dis_pair[0],dis_pair[1])
+        else:
+            coor = self.car_now_coor
+
+        if length != 0:
+            if direction == 'front':
+                angle = 0
+            elif direction == 'left front':
+                angle = pi/4
+            elif direction == 'left':
+                angle = pi/2
+            elif direction == 'left back':
+                angle = pi*3/4
+            elif direction == 'back':
+                angle = pi
+            elif direction == 'right back':
+                angle = pi*5/4
+            elif direction == 'right':
+                angle = pi*3/2
+            elif direction == 'right front':
+                angle = pi*7/4
+            else:
+                print('error, no this direction')
+                return False
+
+            rotate_matrix = np.matrix([[cos(angle),-sin(angle)],[sin(angle),cos(angle)]])
+
+            tmp_direction = rotate_matrix * self.car_direction
+
+            coor += length * tmp_direction
+        
+        return self.map[coor[0][0]][coor[1][0]] == MAP_NOT_GET_HERE
     
-    def __get_coordination(self,dis_a:float,dis_b:float) -> list[int]:
+    def __get_coordination(self,dis_a:float,dis_b:float) -> np.mat:
         coor = self.__cal_location(dis_a,dis_b)
         y = int(coor[1]/BLOCK_SIZE) + self.server_a_coor[1]
         x = int(coor[0]/BLOCK_SIZE) + self.server_a_coor[0]
 
-        return (x,y)
+        return np.mat([[x],[y]])
 
     def __cal_location(self,dis_a:float,dis_b:float):
         y = (self.servers_distance ** 2 + dis_a ** 2 - dis_b ** 2) / ( 2 * self.servers_distance )
         x = (dis_a ** 2 - y ** 2) ** (1/2)
         return (x,y)
 
-    #def is_cross_obs(self,dis_a,dis_b) -> bool:
-    #    coor = self.__get_coordination(dis_a,dis_b)
-    #    for i in range(len(self.obstacle[0])):
-    #        if (coor[0] - self.obstacle[0][i]) ** 2 + (coor[1] - self.obstacle[1][i]) ** 2 <= 225:
-    #            return True
-    #        
-    #    return False
-    #
+    
+
     #def get_path(self,start_point,end_point):
     #    start_coor = self.__get_coordination(start_point[0],start_point[1])
     #    end_coor = self.__get_coordination(end_point[0],end_point[1])
